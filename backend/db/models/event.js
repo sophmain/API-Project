919926ef7ac5a692e3ce8c3a0af0b43f93cfc1a2
Sophie Main <sophiekmain@gmail.com>
2022-12-19 15:26:56 -0600

    Migration and model files added

---
+++ backend/db/models/event.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Event.init({
+    venueId: DataTypes.INTEGER,
+    groupId: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    type: {
+      type: DataTypes.ENUM,
+      values: ['Online', 'In Person']
+    },
+    capacity: DataTypes.INTEGER,
+    price: DataTypes.INTEGER,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Event',
+  });
+  return Event;
+};
