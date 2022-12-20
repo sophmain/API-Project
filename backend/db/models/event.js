008a7b653d65c02016a96c96fce4de299a2a1dd8
Sophie Main <sophiekmain@gmail.com>
2022-12-20 08:22:50 -0600

    Added seed data to attendances table

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -11,6 +11,18 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Event.belongsToMany(models.User, {
+        through: models.Attendance
+      })
+      Event.belongsTo(models.Venue, {
+        foreignKey: 'venueId'
+      })
+      Event.belongsTo(models.Group, {
+        foreignKey: 'groupId'
+      })
+      Event.hasMany(models.EventImage, {
+        foreignKey: 'eventId'
+      })
     }
   }
   Event.init({
