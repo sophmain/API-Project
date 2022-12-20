0e86e80b3c1855da8f496c2f1d5d86e18477e6a1
Sophie Main <sophiekmain@gmail.com>
2022-12-20 13:18:33 -0600

    add options to migration files

--- backend/db/migrations/20221219190403-create-event-images.js
+++ backend/db/migrations/20221219190403-create-event-images.js
@@ -1,5 +1,9 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('EventImages', {
@@ -31,9 +35,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('EventImages');
+    options.tableName = "EventImages";
+    await queryInterface.dropTable(options);
   }
 };
