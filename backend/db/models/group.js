c1316e81b1001ed3941488468ef44afed1b52f6f
Sophie Main <sophiekmain@gmail.com>
2022-12-21 09:30:37 -0600

    enum fix

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -55,7 +55,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     type: {
       type: DataTypes.ENUM,
-      values: ['In person', 'Online']
+      values: ['Online','In person']
       /*'New Groups',
       'Art & Culture', 'Career & Business',
       'Community & Environment', 'Dancing',
